43120a93821caa84aa9426f0926b0c79c3c0f8f5
Avrami Hammer <avrami.hammer@rmit.edu.au>
2019-09-25 17:10:37 +1000

    Add dynamic api url

--- src/FetchData.js
+++ src/FetchData.js
@@ -13,14 +13,14 @@ class FetchData extends Component {
 
     getSyncTimeout = (query_frequency) => {
         let params = new URLSearchParams(window.location.search);
-        if(params.has("sync_key")) {
+        if (params.has("sync_key")) {
             try {
                 let common_date = new Date(parseInt(params.get("sync_key")));
 
                 let now = new Date();
                 let ms = now.valueOf() - common_date.valueOf();
                 return query_frequency - (ms % query_frequency)
-            }catch (e) {
+            } catch (e) {
                 console.log(e);
             }
         }
@@ -50,18 +50,60 @@ class FetchData extends Component {
         }
     }
 
+    buildTimeFilter(from, until) {
+        let params = new URLSearchParams(window.location.search);
+        if (until === undefined) {
+            if(params.has("end_at")){
+                until= new Date(params.get("end_at"))
+            }else {
+                until = new Date()
+            }
+        }
+        if (from === undefined) {
+            if (params.has("start_at")) {
+                from = new Date(params.get("start_at"));
+            } else {
+                let window = 30;
+                if(params.has("window")){
+                    window = parseInt(params.get("window"))
+                }
+                from = until;
+                from.setMinutes((until).getMinutes() - window);
+            }
+        }
+        until = until.toUTCString();
+        from = from.toUTCString();
+        return {
+            last_viewed: {"$gte": from, "$lte": until}
+        };
+    }
+
+    buildRecordRequestURL(filter) {
+        let location = process.env.REACT_APP_API_LOCATION || ':8080';
+        const url = new URL(`http://${window.location.hostname}${location}/joint`);
+
+        url.search = new URLSearchParams({
+            "max_results": "200",
+            "page": "1",
+            "sort": "-last_view",
+            "where": JSON.stringify(filter)
+        });
+        return url
+    }
 
     fetchData = () => {
-        fetch(`http://${window.location.hostname}:8080/joint?max_results=200&page=1&sort=-last_view`).then(
+        fetch(this.buildRecordRequestURL(this.buildTimeFilter()).toString()).then(
             response => {
                 return response.json()
             }
         ).then(
             data => {
                 this.setState({
-                        books: data["_items"].map(book => <BookCard key={book['_id']} book={book}/>),
+                        books: data["_items"].map(book => < BookCard key={book['_id']} book={book}
+                        />),
                         connected: true,
-                        last_beacon: new Date()
+                        last_beacon:
+                            new Date()
                     }
                 )
             }
@@ -77,7 +119,10 @@ class FetchData extends Component {
     render() {
         return <Gallery>
             {this.state.books && this.state.books}
-            <Header online={this.state.connected} when={this.state.last_beacon}/>
+            <Header
+                online={this.state.connected}
+                when={this.state.last_beacon}
+            />
         </Gallery>
     }
 
